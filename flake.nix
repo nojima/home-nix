b46d68c34ef56a933c07aa2f1e2845bfc92c4078
Yusuke Nojima <nojima@ynojima.com>
2025-01-08 14:09:18 +0900

    Revert "Use specified Go version"
    This reverts commit 5d9c8c895a5ae88e03e58769d98a1f9b02302b35.

--- flake.nix
+++ flake.nix
@@ -13,27 +13,7 @@
   outputs = { nixpkgs, home-manager, ... }:
     let
       system = "x86_64-linux";
-      # Go関連のツールを指定したバージョンのものに差し替える
-      goOverlay = self: super: {
-        go = super.callPackage ./go/go.nix {
-          version = "1.23.4";
-          sha256 = "1y0vq1nxjzk0qabdkfh31kasnz2xafk9g24mq5aaf2wx15f306j2";
-        };
-        gotools = self.callPackage ./go/gotools.nix {
-          version = "0.29.0";
-          hash = "sha256-h3UjRY1w0AyONADNiLhxXt9/z7Tb/40FJI8rKGXpBeM=";
-          vendorHash = "sha256-5JE4/2yn5emRuq4wGTeNnAJa+QZayDq2n+dL3gMML/M=";
-        };
-        gopls = self.callPackage ./go/gopls.nix {
-          version = "0.17.1";
-          hash = "sha256-NLUIFNooOOA4LEL5nZNdP9TvDkQUqLjKi44kZtOxeuI=";
-          vendorHash = "sha256-wH3YRiok3YWNzw9ejXMMitq58SxrNWXiKYKz2Hf0ZlM=";
-        };
-      };
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [ goOverlay ];
-      };
+      pkgs = nixpkgs.legacyPackages.${system};
     in {
       homeConfigurations."nojima" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
